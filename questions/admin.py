d3ef4c20bf94831b71229d479a9a5533de8d20d8
neelansh <neelansh5@gmail.com>
2016-03-21 23:54:40 +0530

    added answers model

--- questions/admin.py
+++ questions/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question , Answers
 
 # Register your models here.
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
+    list_display = ["title" , "created_by"]
+
+@admin.register(Answers)
+class AnswerAdmin(admin.ModelAdmin):
     list_display = ["title" , "created_by"]
--- questions/models.py
+++ questions/models.py
@@ -7,6 +7,17 @@ class Question(models.Model):
     text = models.TextField(max_length = 1024 , default = '')
     create_on = models.DateTimeField(auto_now_add = True)
     created_by = models.ForeignKey(MyUser , related_name = 'question_created')
-    upvoted_by = models.ManyToManyField(MyUser , related_name = 'question_upvoted' , blank = True , null = True)
+    upvoted_by = models.ManyToManyField(MyUser , related_name = 'question_upvoted' , blank = True)
+    def __str__(self):
+        return self.title
+
+
+class Answers(models.Model):
+    title = models.CharField(max_length = 100 , default = "")
+    text = models.TextField(max_length = 1024 , default = "")
+    created_by = models.ForeignKey(MyUser , related_name = "answer_created")
+    ques_id = models.ForeignKey(Question , related_name = "question_answer")
+    created_on = models.DateField(auto_now_add = True)
+    upvoted_by = models.ManyToManyField(MyUser , related_name = "answer_upvoted" , blank = True)
     def __str__(self):
         return self.title
