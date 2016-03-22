02fb8fc37bdfa05aabc519d8f889f5f83cfe3d5e
neelansh <neelansh5@gmail.com>
2016-03-23 01:05:55 +0530

    changed static

--- questions/views.py
+++ questions/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_GET, require_POST,require_http_methods
-
+from .models import Question
 
 # Create your views here.
 @require_GET
 def display_ques(request):
-    return render(request , "questions/display_questions.html");
+    context = { "questions" : Question.objects.all()}
+    return render(request , "questions/display_questions.html" , context);
